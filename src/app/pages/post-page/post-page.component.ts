245571d3c4e8c8be2eec1e23473ae3120618c9fb
Maxim «ˈLiːbrə» Pavlenko <maxim.mephistos@gmail.com>
2020-11-06 16:57:32 +0300

    Finished Function Part All App

--- src/app/pages/post-page/post-page.component.ts
+++ src/app/pages/post-page/post-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import Post from 'src/app/shared/interfaces/post';
+import { PostsService } from './../../shared/services/posts.service';
 
 @Component( {
   selector: 'app-post-page',
@@ -7,9 +12,18 @@ import { Component, OnInit } from '@angular/core';
 } )
 export class PostPageComponent implements OnInit {
 
-  constructor () { }
+  public post$: Observable<Post>;
+
+  constructor (
+    private postsService: PostsService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.post$ = this.route.params
+      .pipe( switchMap( ( params: Params ) => {
+        return this.postsService.getById( params[ 'id' ] );
+      } ) );
   }
 
 }
